ab725f1fb96028f1990766284a0cdd753bd4a8fd
wumch <wumuch@gmail.com>
2017-09-07 06:26:12 +0800

    keras regression

--- research/model/bp.R
+++ research/model/bp.R
@@ -23,3 +23,20 @@ MSE <- sum((samples.test.sold - pred.unnormed[,1]) ^ 2) / length(samples.test.so
 par(mfrow=c(1,2))
 plot(samples.test.sold, pred.unnormed, col='red', main='samples vs predict', pch=18, cex=0.7)
 abline(0, 1, lwd=2)
+
+
+library(RSNNS)
+inputs <- as.matrix(seq(0,10,0.1))
+outputs <- as.matrix(sin(inputs) + runif(inputs*0.2))
+outputs <- normalizeData(outputs, "0_1")
+model <- rbf(inputs, outputs, size=40, maxit=1000,
+             initFuncParams=c(0, 1, 0, 0.01, 0.01),
+             learnFuncParams=c(1e-8, 0, 1e-8, 0.1, 0.8),
+             linOut=TRUE)
+par(mfrow=c(2,1))
+plotIterativeError(model)
+plot(inputs, outputs)
+lines(inputs, fitted(model), col="green")
+
+RBF_Weights_Kohonen(0,0,0,0,0) 
+
---
+++ src/core/DNN.py
@@ -0,0 +1,92 @@
+#!/data/pyenv/keras/bin/python
+
+import sys
+import os
+import numpy as np
+import pandas
+from keras.models import Sequential
+from keras.optimizers import adam
+from keras.layers.core import Dense, Activation
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Lasso
+
+
+data_path = os.path.join(
+    os.path.dirname(
+        os.path.dirname(os.path.dirname(__file__))), 'data')
+# data_file = os.path.join(data_path, '57_0811-0821-sold-tbsold-rate.ssv')
+data_file = os.path.join(data_path, 'generated.ssv')
+model_weights_output = os.path.join(data_path, 'DNN.model')
+
+
+def gen_data():
+    with open(data_file, 'w') as fp:
+        fp.write('sold\tclick\ttbsold' + os.linesep)
+        y = np.random.rand(200)
+        x1 = np.sin(y)
+        x2 = np.random.standard_normal(200)
+        y.reshape(-1)
+        x1.reshape(-1)
+        x2.reshape(-1)
+        for i in range(y.shape[0]):
+            fp.write('%f\t%f\t%f%s' % (y[i], x1[i], x2[i], os.linesep))
+        fp.flush()
+# gen_data()
+
+data_frame = pandas.read_table(data_file, delim_whitespace=True, header='infer')
+lasso = Lasso(alpha=1)
+lasso.fit(data_frame.iloc[:, 1:3], data_frame['sold'])
+print(os.linesep, '<Lasso>.coef: ', lasso.coef_)
+
+dataset = data_frame.values
+data_input = dataset[:, 1:3]
+data_output = dataset[:, 0].reshape(-1, 1)
+data_input = StandardScaler().fit(data_input).transform(data_input)
+data_output = StandardScaler().fit(data_output).transform(data_output)
+train_input = data_input[0:180, :]
+train_output = data_output[0:180, :]
+test_input = data_input[180:, :]
+test_output = data_output[180:, :]
+
+
+def build_model():
+    model = Sequential()
+    model.add(Dense(5, input_dim=2))
+    model.add(Activation('relu'))
+    model.add(Dense(3))
+    model.add(Activation('relu'))
+    model.add(Dense(1))
+    # model.add(Activation('linear'))
+    optimizer = adam(lr=0.001)
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
+    return model
+
+random_seed = 2
+np.random.seed(random_seed)
+
+model = build_model()
+model.fit(train_input, train_output, batch_size=10, epochs=200, verbose=0, validation_split=0.3)
+predict = model.predict(test_input, batch_size=1)
+print(test_output.reshape(1, -1)[0])
+print(predict.reshape(1, -1)[0])
+
+train_score = model.evaluate(train_input, train_output, batch_size=1)
+test_score = model.evaluate(test_input, test_output, batch_size=1)
+mse = ((predict - test_output) ** 2).sum() / predict.shape[0]
+std = (np.abs(predict - test_output)).sum() / np.abs(test_output - test_output.mean()).sum()
+print(os.linesep)
+print(os.linesep, 'train score: %.2f' % (train_score * 100))
+print(os.linesep, 'test score:  %.2f' % (test_score * 100))
+print(os.linesep, 'mse: %.2f, std: %.2f' % (mse, std), os.linesep)
+sys.exit(0)
+
+# model.save_weights(model_weights_output)
+
+
+estimater = KerasRegressor(
+    build_fn=build_model, epochs=100, batch_size=5, verbose=0,
+    validation_split=0.15)
+kfold = KFold(n_splits=10, random_state=random_seed)
+result = cross_val_score(estimater, data_input, data_output, cv=kfold)
+print("Results: %.2f (%.2f) MSE" % (result.mean(), result.std()))
+
