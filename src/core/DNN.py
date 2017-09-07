f212e6addd0c0f6fd5fec890b3561c40425b973f
wumch <wumuch@gmail.com>
2017-09-07 19:03:56 +0800

    fitted

--- src/core/DNN.py
+++ src/core/DNN.py
@@ -7,9 +7,11 @@ import pandas
 from keras.models import Sequential
 from keras.optimizers import adam
 from keras.layers.core import Dense, Activation
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, scale
 from sklearn.linear_model import Lasso
 
+random_seed = 2
+np.random.seed(random_seed)
 
 data_path = os.path.join(
     os.path.dirname(
@@ -21,15 +23,13 @@ model_weights_output = os.path.join(data_path, 'DNN.model')
 
 def gen_data():
     with open(data_file, 'w') as fp:
-        fp.write('sold\tclick\ttbsold' + os.linesep)
-        y = np.random.rand(200) * 3.78
-        x1 = np.sin(y)
-        x2 = np.random.standard_normal(200)
+        fp.write('sold\tclick' + os.linesep)
+        x = (np.random.rand(2000) - 1) * 6
+        y = np.sin(x)
         y.reshape(-1)
-        x1.reshape(-1)
-        x2.reshape(-1)
+        x.reshape(-1)
         for i in range(y.shape[0]):
-            fp.write('%f\t%f\t%f%s' % (y[i], x1[i], x2[i], os.linesep))
+            fp.write('%f\t%f%s' % (y[i], x[i], os.linesep))
         fp.flush()
 gen_data()
 
@@ -39,44 +39,45 @@ lasso.fit(data_frame.iloc[:, 1:3], data_frame['sold'])
 print(os.linesep, '<Lasso>.coef: ', lasso.coef_)
 
 dataset = data_frame.values
-data_input = dataset[:, 1:3]
+data_input = dataset[:, 1:2]
 data_output = dataset[:, 0].reshape(-1, 1)
-data_input = StandardScaler().fit(data_input).transform(data_input)
-data_output = StandardScaler().fit(data_output).transform(data_output)
-train_input = data_input[0:180, :]
-train_output = data_output[0:180, :]
-test_input = data_input[180:, :]
-test_output = data_output[180:, :]
+
+train_input = data_input[0:1880, :]
+train_output = data_output[0:1880, :]
+test_input = data_input[1880:, :]
+test_output = data_output[1880:, :]
+
+# train_input = scale(train_input)
+# scaler = StandardScaler().fit(train_input)
+# train_input = scaler.transform(train_input)
+# test_input = scaler.transform(test_input)
 
 
 def build_model():
     model = Sequential()
-    model.add(Dense(5, input_dim=2))
+    model.add(Dense(100, kernel_initializer='uniform', input_dim=1))
     model.add(Activation('relu'))
-    model.add(Dense(3))
+    model.add(Dense(50))
     model.add(Activation('relu'))
     model.add(Dense(1))
-    # model.add(Activation('linear'))
+    model.add(Activation('tanh'))
     optimizer = adam(lr=0.001)
-    model.compile(optimizer=optimizer, loss='mean_squared_error')
+    model.compile(optimizer='rmsprop', loss='mean_squared_error')
     return model
 
-random_seed = 2
-np.random.seed(random_seed)
-
 model = build_model()
-model.fit(train_input, train_output, batch_size=10, epochs=200, verbose=0, validation_split=0.3)
+model.fit(train_input, train_output, batch_size=10, epochs=100, shuffle=True, verbose=0, validation_split=0.)
 predict = model.predict(test_input, batch_size=1)
 print(test_output.reshape(1, -1)[0])
 print(predict.reshape(1, -1)[0])
 
-train_score = model.evaluate(train_input, train_output, batch_size=1)
-test_score = model.evaluate(test_input, test_output, batch_size=1)
+train_loss = model.evaluate(train_input, train_output, batch_size=1)
+test_loss = model.evaluate(test_input, test_output, batch_size=1)
 mse = ((predict - test_output) ** 2).sum() / predict.shape[0]
 std = (np.abs(predict - test_output)).sum() / np.abs(test_output - test_output.mean()).sum()
 print(os.linesep)
-print(os.linesep, 'train score: %.2f' % (train_score * 100))
-print(os.linesep, 'test score:  %.2f' % (test_score * 100))
+print(os.linesep, 'train score: %.2f' % (train_loss * 100))
+print(os.linesep, 'test score:  %.2f' % (test_loss * 100))
 print(os.linesep, 'mse: %.2f, std: %.2f' % (mse, std), os.linesep)
 sys.exit(0)
 
